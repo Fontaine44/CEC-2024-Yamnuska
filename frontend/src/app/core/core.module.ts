a3e603f5bc688c198b95c6acac5b66f77889d2e0
Fontaine44 <raphael.fontaine@mail.mcgill.ca>
2024-03-02 17:05:15 -0700

    Incorporate frontend

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { Algo2Component } from './components/algo2/algo2.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PlotlyModule } from 'angular-plotly.js';
-import { Example1Component } from './components/exemple1/example1.component';
-import { Algo4Component } from './components/algo4/algo4.component';
-import { Algo3Component } from './components/algo3/algo3.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { CustomComponent } from './components/custom/custom.component';
+import { Example1Component } from './components/example1/example1.component';
 
 
 const routes: Routes = [
@@ -17,21 +15,13 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'example1',
+    path: 'example',
     component: Example1Component,
   },
   {
-    path: 'Algo2',
-    component: Algo2Component,
-  },
-  {
-    path: 'Algo3',
-    component: Algo3Component,
-  },
-  {
-    path: 'Algo4',
-    component: Algo4Component,
-  },
+    path: 'custom',
+    component: CustomComponent,
+  }
 ];
 
 
@@ -39,9 +29,7 @@ const routes: Routes = [
   declarations: [
     HomeComponent,
     Example1Component,
-    Algo2Component,
-    Algo3Component,
-    Algo4Component,
+    CustomComponent,
   ],
   imports: [
     CommonModule,
